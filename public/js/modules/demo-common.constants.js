24e494de97cb58a07a4ecd866ccb8c2fad48dd25
Thomas D'Ascoli <thomas@dasco.li>
2014-11-11 16:38:36 +0100

    v0.0.16-poc

--- public/js/modules/demo-common.constants.js
+++ public/js/modules/demo-common.constants.js
@@ -2,7 +2,7 @@ define(['angular'], function () {
 
     var module = angular.module('demo-common.constants', []);
 
-    module.constant('applicationVersion', 'v1.0.0 - Demo Application for alv-ch/alv-ch-ng');
+    module.constant('applicationVersion', 'alv-ch-ng v0.0.16-poc - Demo Application for alv-ch/alv-ch-ng');
     module.constant('applicationReleaseNote', 'demo');
 
 });
