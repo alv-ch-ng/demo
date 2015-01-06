e675a0873dc47f64807ea9059a53617c9a48f764
Thomas D'Ascoli <thomas@dasco.li>
2015-01-06 17:29:48 +0100

    fixed package.json

--- src/js/modules/alv-ch-ng.demo.common.constants.js
+++ src/js/modules/alv-ch-ng.demo.common.constants.js
@@ -3,7 +3,7 @@
 
     var module = angular.module('alv-ch-ng.demo.common.constants', []);
 
-    module.constant('applicationVersion', '1.0.0');
+    module.constant('applicationVersion', 'v1.0.0 - demo site for github/alv-ch/alv-ch-ng');
     module.constant('applicationReleaseNote', 'poc');
     module.constant('apiServerBasePath', 'http://localhost:8080/api/');
 //    module.constant('apiServerBasePath', 'http://med-test5.tcsb.admin.ch:8080/api/');
